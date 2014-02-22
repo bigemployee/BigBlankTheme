617310315e4003a2406af27fd082c8a34e9184ab
Norik Davtian <norik@bigemployee.com>
2014-02-22 01:15:30 -0800

    no ie conditional comments IE conditional Stylesheets instead of ugly head
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- header.php
+++ header.php
@@ -5,16 +5,9 @@
  * Displays all of the <head> section and everything up till <div id="main">
  *
  */
-?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <!--<![endif]-->
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <title><?php wp_title('|', true, 'right'); ?></title>
