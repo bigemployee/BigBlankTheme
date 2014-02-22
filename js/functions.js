337a803937f1497f7593add6ffac6468d85131a5
Norik Davtian <norik@bigemployee.com>
2014-02-22 08:18:39 -0800

    remove featured content support cleanup attempt 2
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- js/functions.js
+++ js/functions.js
@@ -116,13 +116,4 @@
         });
     }
 
-    // Initialize Featured Content slider.
-    _window.load(function() {
-        if (body.is('.slider')) {
-            $('.featured-content').featuredslider({
-                selector: '.featured-content-inner > article',
-                controlsContainer: '.featured-content'
-            });
-        }
-    });
 })(jQuery);
