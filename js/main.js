b5cbee1c36605fc0b6f8d2ce2742561d0b1774f2
Norik Davtian <norik@bigemployee.com>
2014-02-22 09:20:06 -0800

    keyboard image navigation bug fix
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- js/main.js
+++ js/main.js
@@ -10,5 +10,23 @@
     }
 
     // jQuery stuff goes here
+    
+    $(document).on('keydown.bigblank', function(e) {
+        var url = false;
+
+        // Left arrow key code.
+        if (e.which === 37) {
+            url = $('.previous-image').parent().attr('href');
+
+            // Right arrow key code.
+        } else if (e.which === 39) {
+            url = $('.next-image').parent().attr('href');
+        }
+
+        if (url && (!$('textarea, input').is(':focus'))) {
+            window.location = url;
+        }
+    });
+    
 
 })(window.jQuery, window, window.document);
