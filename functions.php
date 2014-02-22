a8f12f715697cf00afa0ef2b891dd0d57c442c38
Norik Davtian <norik@bigemployee.com>
2014-02-22 12:54:07 -0800

    remove p tags wrapping images
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- functions.php
+++ functions.php
@@ -220,6 +220,17 @@ function bigblank_scripts() {
 
 add_action('wp_enqueue_scripts', 'bigblank_scripts');
 
+/**
+ *  filter p tags
+ */
+function filter_ptags_on_images($content) {
+    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
+
+
+
 if (!function_exists('bigblank_the_attached_image')) :
 
     /**
