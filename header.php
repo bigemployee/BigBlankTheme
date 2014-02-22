97e59ae2ef6e90eb6674554d6117447e2968f8eb
Norik Davtian <norik@bigemployee.com>
2014-02-22 00:56:45 -0800

    cleanup head load html5 from Google CDN
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- header.php
+++ header.php
@@ -17,13 +17,9 @@
     <!--<![endif]-->
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width">
         <title><?php wp_title('|', true, 'right'); ?></title>
-        <link rel="profile" href="http://gmpg.org/xfn/11">
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-        <!--[if lt IE 9]>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-        <![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!--[if lte IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
