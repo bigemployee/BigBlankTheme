e05aa7cf8655fef5464f469bdb2407df542c7a40
Norik Davtian <norik@bigemployee.com>
2014-02-22 09:16:27 -0800

    previous link bug
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- js/keyboard-image-navigation.js
+++ js/keyboard-image-navigation.js
@@ -7,11 +7,11 @@
 
         // Left arrow key code.
         if (e.which === 37) {
-            url = $('.previous-image a').attr('href');
+            url = $('.previous-image').parent().attr('href');
 
             // Right arrow key code.
         } else if (e.which === 39) {
-            url = $('.entry-attachment a').attr('href');
+            url = $('.next-image').parent().attr('href');
         }
 
         if (url && (!$('textarea, input').is(':focus'))) {
