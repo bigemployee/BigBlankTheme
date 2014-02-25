8870dfcb2fc7e5213a22f2329f53ef6378841bc3
Norik Davtian <norik@bigemployee.com>
2014-02-25 01:22:22 -0800

    init custom post types
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

---
+++ inc/custom-post-types.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom post types are new post types you can create. A custom post type can 
+ * be added to WordPress via the register_post_type() function. This function 
+ * allows you to define a new post type by its labels, supported features, 
+ * availability and other specifics.
+ * 
+ * @link http://codex.wordpress.org/Post_Types#Custom_Post_Types
+ */
