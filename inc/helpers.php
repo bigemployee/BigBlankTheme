8586c808b298c8a67d5ddbb2fe08b214962b007e
Norik Davtian <norik@bigemployee.com>
2014-02-24 19:03:20 -0800

    h2 hashtag and shortcodes
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,25 +1,26 @@
 <?php
+
 /**
  * Functions to facilitate content manipulation and shortcodes
  */
+
 /**
  * A simple button shortcode, with option to pass link and additional class
  * [button link="http://bigemployee.com/" class="big"]Big Employee[/button]
  * Output:
  * <a href="http://bigemployee.com/" class="button big">Big Employee</a>
  */
-function bigblank_add_shortcode_button($atts, $content = null) {
+function be_add_shortcode_button($atts, $content = null) {
     extract(
             shortcode_atts(
                     array(
         'link' => '#',
         'class' => 'button',
                     ), $atts));
-    $content = parse_shortcode_content($content);
-    return bigblank_render_button($content, $link, false, $class);
+    return be_render_button($content, $link, false, $class);
 }
 
-function bigblank_render_button($content = 'new link', $link = '#', $echo = true, $class = 'button') {
+function be_render_button($content = 'new link', $link = '#', $echo = true, $class = 'button') {
     $class = strip_tags(trim($class));
     if (strpos($class, 'button') === FALSE) {
         $class = 'button ' . $class;
@@ -29,12 +30,11 @@ function bigblank_render_button($content = 'new link', $link = '#', $echo = true
     echo '<a href="' . $link . '" class="' . $class . '">' . do_shortcode($content) . '</a>';
 }
 
-add_shortcode('button', 'bigblank_add_shortcode_button');
+add_shortcode('button', 'be_add_shortcode_button');
 
 // This function adds nice anchor with id attribute to our h2 tags for reference
 // Ref: http://www.w3.org/TR/html4/struct/links.html#h-12.2.3
-
-function replace_content($content) {
+function anchor_content_h2($content) {
 
     // Pattern that we want to match
     $pattern = '/<h2>(.*?)<\/h2>/';
@@ -43,24 +43,25 @@ function replace_content($content) {
     $content = preg_replace_callback($pattern,
             // function to replace the title with an id
             function ($matches) {
-                $title = $matches[1];
-                $slug = sanitize_title_with_dashes($title);
-                return '<h2 id="' . $slug . '">' . $title . '</h2>';
-            }
+        $title = $matches[1];
+        $slug = sanitize_title_with_dashes($title);
+        return '<h2 id="' . $slug . '">' . $title . '</h2>';
+    }
             , $content);
     return $content;
 }
 
-add_filter('the_content', 'replace_content');
+add_filter('the_content', 'anchor_content_h2');
 
 // Hide Email from Spam Bots using a short code place this in your functions file
 // http://codex.wordpress.org/Function_Reference/antispambot
 // [email]john.doe@mysite.com[/email]
 // Output: &#106;&#111;h&#110;&#46;&#100;&#111;&#101;&#64;mysit&#101;.&#99;&#111;&#109;
-function HideMail($atts , $content = null ){
-	if ( ! is_email ($content) )
-		return;
+function HideMail($atts, $content = null) {
+    if (!is_email($content))
+        return;
 
-	return '<a href="mailto:'.antispambot($content).'">'.antispambot($content).'</a>';
+    return '<a href="mailto:' . antispambot($content) . '">' . antispambot($content) . '</a>';
 }
-add_shortcode( 'email','HideMail');
+
+add_shortcode('email', 'HideMail');
