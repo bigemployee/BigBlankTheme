9dcef8c343c943e65163bcbe86e1559ef25c83f0
Norik Davtian <norik@bigemployee.com>
2014-02-25 01:27:29 -0800

    less dramatic header, will revisit here again
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- header.php
+++ header.php
@@ -20,22 +20,18 @@
     </head>
     <body <?php body_class(); ?>>
         <div id="page" class="hfeed site">
-            <header id="masthead" class="site-header" role="banner">
-                <div class="header-main">
-                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-                    <div class="search-toggle">
-                        <a href="#search-container" class="screen-reader-text"><?php _e('Search', 'bigblank'); ?></a>
-                    </div>
-                    <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-                        <h1 class="menu-toggle"><?php _e('Primary Menu', 'bigblank'); ?></h1>
-                        <a class="screen-reader-text skip-link" href="#content"><?php _e('Skip to content', 'bigblank'); ?></a>
-                        <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu')); ?>
-                    </nav>
-                </div>
-                <div id="search-container" class="search-box-wrapper hide">
-                    <div class="search-box">
-                        <?php get_search_form(); ?>
-                    </div>
-                </div>
-            </header><!-- #masthead -->
+            <header id="header" class="site-header">
+                <!--  if you would like to use logo instead of site title, you could reuse this commented code -->
+                
+                    <a id="logo" href="<?php echo home_url(); ?>" rel="home">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/bigblanktheme_logo.png" alt="<?php bloginfo('name'); ?> logo" width="200" height="29"/>
+                    </a>
+                
+                <!--<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>-->
+                <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+                    <h1 id="menu-toggle"><i class="fa fa-bars"></i><?php _e('Primary Menu', 'bigblank'); ?></h1>
+                    <a class="screen-reader-text skip-link" href="#content"><?php _e('Skip to content', 'bigblank'); ?></a>
+                    <?php bigblank_main_menu(); ?>
+                </nav>
+            </header><!-- #header -->
             <div id="main" class="site-main">
