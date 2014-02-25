79721a7f58936546042fe294c7cb20ecda0720b8
Norik Davtian <norik@bigemployee.com>
2014-02-25 01:23:50 -0800

    sidebar rename
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- sidebar-content.php
+++ sidebar-content.php
@@ -3,10 +3,10 @@
  * The Content Sidebar
  *
  */
-if (!is_active_sidebar('sidebar-2')) {
+if (!is_active_sidebar('sidebar')) {
     return;
 }
 ?>
 <div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
-    <?php dynamic_sidebar('sidebar-2'); ?>
+    <?php dynamic_sidebar('sidebar'); ?>
 </div><!-- #content-sidebar -->
