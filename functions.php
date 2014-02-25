b527aa5cbe0eecff7ce655c8cb2d9dcdd759fd89
Norik Davtian <norik@bigemployee.com>
2014-02-24 20:11:22 -0800

    Refactoring  - admin specific helpers have admin prefix  - filters  - shortcodes  - widgets and sidebars  - custom post types
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ if (!isset($content_width)) {
  * Big Blank only works in WordPress 3.8 or later.
  */
 if (version_compare($GLOBALS['wp_version'], '3.8', '<')) {
-    require get_template_directory() . '/inc/back-compat.php';
+    require get_template_directory() . '/inc/admin-back-compat.php';
 }
 if (!function_exists('bigblank_setup')) :
 
@@ -118,18 +118,6 @@ function be_head_cleanup() {
 add_action('init', 'be_head_cleanup');
 
 /**
- * Remove recent comments inline style form WP <head>
- * .recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}
- * @global type $wp_widget_factory
- */
-function be_remove_recent_comments_style() {
-    global $wp_widget_factory;
-    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
-}
-
-add_action('widgets_init', 'be_remove_recent_comments_style');
-
-/**
  * Remove version from CSS and JS files for Caching
  * @param string|array $src Query key or keys to remove.
  * @return string New URL query string.
@@ -143,28 +131,6 @@ function bigblank_remove_wp_ver_css_js($src) {
 add_filter('style_loader_src', 'bigblank_remove_wp_ver_css_js');
 add_filter('script_loader_src', 'bigblank_remove_wp_ver_css_js');
 
-/**
- * Register three Big Blank widget areas.
- *
- *
- * @return void
- */
-function bigblank_widgets_init() {
-//    require get_template_directory() . '/inc/widgets.php';
-//    register_widget('Big_Blank_Button_Widget');
-    register_sidebar(array(
-        'name' => __('Primary Sidebar', 'bigblank'),
-        'id' => 'sidebar',
-        'description' => __('Sidebar appears next to content.', 'bigblank'),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h4 class="widget-title">',
-        'after_title' => '</h4>',
-    ));
-
-}
-
-add_action('widgets_init', 'bigblank_widgets_init');
 
 /**
  * Enqueue scripts and styles for the front end.
@@ -203,218 +169,17 @@ function bigblank_scripts() {
 
 add_action('wp_enqueue_scripts', 'bigblank_scripts');
 
-/**
- *  filter <p> tags wrapping images
- */
-function filter_ptags_on_images($content) {
-    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-}
+// Widgets and Sidebars
+require get_template_directory() . '/inc/widgets-sidebars.php';
 
-add_filter('the_content', 'filter_ptags_on_images');
+// Custom post types
+require get_template_directory() . '/inc/custom-post-types.php';
 
-/**
- * Set Excerpt length
- * @param int $length
- * @return int set post excerpt length
- */
-function be_excerpt_length($length) {
-    return 140;
-}
-
-add_filter('excerpt_length', 'be_excerpt_length');
-
-if (!function_exists('bigblank_the_attached_image')) :
-
-    /**
-     * Print the attached image with a link to the next attached image.
-     *
-     *
-     * @return void
-     */
-    function bigblank_the_attached_image() {
-        $post = get_post();
-        /**
-         * Filter the default Big Blank attachment size.
-         *
-         *
-         * @param array $dimensions {
-         *     An array of height and width dimensions.
-         *
-         *     @type int $height Height of the image in pixels. Default 810.
-         *     @type int $width  Width of the image in pixels. Default 810.
-         * }
-         */
-        $attachment_size = apply_filters('bigblank_attachment_size', array(810, 810));
-        $next_attachment_url = wp_get_attachment_url();
-        /*
-         * Grab the IDs of all the image attachments in a gallery so we can get the URL
-         * of the next adjacent image in a gallery, or the first image (if we're
-         * looking at the last image in a gallery), or, in a gallery of one, just the
-         * link to that image file.
-         */
-        $attachment_ids = get_posts(array(
-            'post_parent' => $post->post_parent,
-            'fields' => 'ids',
-            'numberposts' => -1,
-            'post_status' => 'inherit',
-            'post_type' => 'attachment',
-            'post_mime_type' => 'image',
-            'order' => 'ASC',
-            'orderby' => 'menu_order ID',
-        ));
-        // If there is more than 1 attachment in a gallery...
-        if (count($attachment_ids) > 1) {
-            foreach ($attachment_ids as $attachment_id) {
-                if ($attachment_id == $post->ID) {
-                    $next_id = current($attachment_ids);
-                    break;
-                }
-            }
-            // get the URL of the next image attachment...
-            if ($next_id) {
-                $next_attachment_url = get_attachment_link($next_id);
-            }
-            // or get the URL of the first image attachment.
-            else {
-                $next_attachment_url = get_attachment_link(array_shift($attachment_ids));
-            }
-        }
-        printf('<a href="%1$s" rel="attachment">%2$s</a>', esc_url($next_attachment_url), wp_get_attachment_image($post->ID, $attachment_size)
-        );
-    }
-
-endif;
-if (!function_exists('bigblank_list_authors')) :
-
-    /**
-     * Print a list of all site contributors who published at least one post.
-     *
-     *
-     * @return void
-     */
-    function bigblank_list_authors() {
-        $contributor_ids = get_users(array(
-            'fields' => 'ID',
-            'orderby' => 'post_count',
-            'order' => 'DESC',
-            'who' => 'authors',
-        ));
-        foreach ($contributor_ids as $contributor_id) :
-            $post_count = count_user_posts($contributor_id);
-            // Move on if user has not published a post (yet).
-            if (!$post_count) {
-                continue;
-            }
-            ?>
-            <div class="contributor">
-                <div class="contributor-info">
-                    <div class="contributor-avatar"><?php echo get_avatar($contributor_id, 132); ?></div>
-                    <div class="contributor-summary">
-                        <h2 class="contributor-name"><?php echo get_the_author_meta('display_name', $contributor_id); ?></h2>
-                        <p class="contributor-bio">
-                            <?php echo get_the_author_meta('description', $contributor_id); ?>
-                        </p>
-                        <a class="contributor-posts-link" href="<?php echo esc_url(get_author_posts_url($contributor_id)); ?>">
-                            <?php printf(_n('%d Article', '%d Articles', $post_count, 'bigblank'), $post_count); ?>
-                        </a>
-                    </div><!-- .contributor-summary -->
-                </div><!-- .contributor-info -->
-            </div><!-- .contributor -->
-            <?php
-        endforeach;
-    }
-
-endif;
-
-/**
- * Extend the default WordPress body classes.
- *
- * Adds body classes to denote:
- * 1. Single or multiple authors.
- * 2. Index views.
- * 3. Full-width content layout.
- * 4. Presence of footer widgets.
- * 5. Single views.
- *
- *
- * @param array $classes A list of existing body class values.
- * @return array The filtered body class list.
- */
-function bigblank_body_classes($classes) {
-    if (is_multi_author()) {
-        $classes[] = 'group-blog';
-    }
-    if (is_archive() || is_search() || is_home()) {
-        $classes[] = 'list-view';
-    }
-    if ((!is_active_sidebar('sidebar-2') ) || is_page_template('page-templates/full-width.php') || is_page_template('page-templates/contributors.php') || is_attachment()) {
-        $classes[] = 'full-width';
-    }
-    if (is_active_sidebar('sidebar-3')) {
-        $classes[] = 'footer-widgets';
-    }
-    if (is_singular() && !is_front_page()) {
-        $classes[] = 'singular';
-    }
-    return $classes;
-}
-
-add_filter('body_class', 'bigblank_body_classes');
-
-/**
- * Extend the default WordPress post classes.
- *
- * Adds a post class to denote:
- * Non-password protected page with a post thumbnail.
- *
- *
- * @param array $classes A list of existing post class values.
- * @return array The filtered post class list.
- */
-function bigblank_post_classes($classes) {
-    if (!post_password_required() && has_post_thumbnail()) {
-        $classes[] = 'has-post-thumbnail';
-    }
-    return $classes;
-}
-
-add_filter('post_class', 'bigblank_post_classes');
-
-/**
- * Create a nicely formatted and more specific title element text for output
- * in head of document, based on current view.
- *
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function bigblank_wp_title($title, $sep) {
-    global $paged, $page;
-    if (is_feed()) {
-        return $title;
-    }
-    // Add the site name.
-    $title .= get_bloginfo('name');
-    // Add the site description for the home/front page.
-    $site_description = get_bloginfo('description', 'display');
-    if ($site_description && ( is_home() || is_front_page() )) {
-        $title = "$title $sep $site_description";
-    }
-    // Add a page number if necessary.
-    if ($paged >= 2 || $page >= 2) {
-        $title = "$title $sep " . sprintf(__('Page %s', 'bigblank'), max($paged, $page));
-    }
-    return $title;
-}
-
-add_filter('wp_title', 'bigblank_wp_title', 10, 2);
+// Filters and functions to manipulate content
+require get_template_directory() . '/inc/filters.php';
 
 // Custom template tags for this theme.
 require get_template_directory() . '/inc/template-tags.php';
 
-// Helper functions
-require get_template_directory() . '/inc/helpers.php';
-
 // Add Theme Customizer functionality.
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/admin-customizer.php';
