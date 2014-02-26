9c913d4f65a84bf7630a7985371c8cd816d56d05
Norik Davtian <norik@bigemployee.com>
2014-02-26 09:53:37 -0800

    new team custom post type
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -7,3 +7,53 @@
  * 
  * @link http://codex.wordpress.org/Post_Types#Custom_Post_Types
  */
+function be_register_post_type() {
+    $teamLabels = array(
+        'name' => 'Team',
+        'singular_name' => 'Team',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Profile',
+        'edit_item' => 'Edit Profile',
+        'new_item' => 'New Profile',
+        'all_items' => 'All Profiles',
+        'view_item' => 'View Profile',
+        'search_items' => 'Search Profiles',
+        'not_found' => 'No profile was found',
+        'not_found_in_trash' => 'No profile was found in Trash',
+        'parent_item_colon' => '',
+        'menu_name' => 'Team'
+    );
+
+    $teamArgs = array(
+        'labels' => $teamLabels,
+        'public' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'menu_icon' => 'dashicons-businessman',
+        'query_var' => true,
+        'rewrite' => array('slug' => 'team'),
+        'has_archive' => false,
+        'hierarchical' => false,
+        'supports' => array(
+            'title',
+            'editor',
+            'author',
+            'thumbnail',
+            'excerpt',
+//            'trackbacks',
+            'custom-fields',
+//            'comments',
+            'revisions',
+//            'page-attributes',
+//            'post-formats',
+        )
+    );
+
+    register_post_type('team', $teamArgs);
+    flush_rewrite_rules();
+}
+
+add_action('init', 'be_register_post_type');
