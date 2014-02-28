c37a94df28ea9c9e03721583462b6ae97a85d3ab
Norik Davtian <norik@bigemployee.com>
2014-02-28 13:16:03 -0800

    added some comments for help
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- header.php
+++ header.php
@@ -21,14 +21,21 @@
     <body <?php body_class(); ?>>
         <div id="page" class="hfeed site">
             <header id="header" class="site-header" role="banner">
-                <!--  if you would like to use logo instead of site title, you could reuse this commented code -->
-                
-                    <a id="logo" href="<?php echo home_url(); ?>" rel="home">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/bigblanktheme_logo.png" alt="<?php bloginfo('name'); ?> logo" width="200" height="29"/>
-                    </a>
-                
-                <!--<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>-->
-                <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+                <?php
+                /**
+                 * Use Logo instead of site title
+                 */
+                ?>
+                <a id="logo" href="<?php echo home_url(); ?>" rel="home">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/bigblanktheme_logo.png" alt="<?php bloginfo('name'); ?> logo" width="200" height="29"/>
+                </a>
+                <?php
+                /**
+                 * Use this line to display site title as text, don't foget to comment above lines
+                 * <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                 */
+                ?>
+                <nav id="menu" role="navigation">
                     <h1 id="menu-toggle"><i class="fa fa-bars"></i><?php _e('Primary Menu', 'bigblank'); ?></h1>
                     <a class="screen-reader-text skip-link" href="#content"><?php _e('Skip to content', 'bigblank'); ?></a>
                     <?php bigblank_main_menu(); ?>
