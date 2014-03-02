0d80d42f9238b0adb6de561369cb29223d695a74
Norik Davtian <norik@bigemployee.com>
2014-03-02 07:14:37 -0800

    removed comments that did not apply
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- header-layout.php
+++ header-layout.php
@@ -2,8 +2,6 @@
 /**
  * Adding layout class to our main content
  * Always call get_footer('layout'); to close tags opened here.
- * @todo create a function to overwrite comments open
- * @link https://codex.wordpress.org/Function_Reference/comments_open#Examples
  */
 $layout = bigblank_get_layout();
 ?>
