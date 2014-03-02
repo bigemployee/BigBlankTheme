4506e81c9489634386170a8e1f13163c5ca560d6
Norik Davtian <norik@bigemployee.com>
2014-03-02 06:51:08 -0800

    Removed contributors listing... More like a theme specific feature than general purpose use Will add it later if it was not the right decision, for right now let's cleanup some more
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- inc/filters.php
+++ inc/filters.php
@@ -96,47 +96,6 @@ if (!function_exists('bigblank_the_attached_image')) :
     }
 
 endif;
-if (!function_exists('bigblank_list_authors')) :
-
-    /**
-     * Print a list of all site contributors who published at least one post.
-     *
-     *
-     * @return void
-     */
-    function bigblank_list_authors() {
-        $contributor_ids = get_users(array(
-            'fields' => 'ID',
-            'orderby' => 'post_count',
-            'order' => 'DESC',
-            'who' => 'authors',
-        ));
-        foreach ($contributor_ids as $contributor_id) :
-            $post_count = count_user_posts($contributor_id);
-            // Move on if user has not published a post (yet).
-            if (!$post_count) {
-                continue;
-            }
-            ?>
-            <div class="contributor">
-                <div class="contributor-info">
-                    <div class="contributor-avatar"><?php echo get_avatar($contributor_id, 132); ?></div>
-                    <div class="contributor-summary">
-                        <h2 class="contributor-name"><?php echo get_the_author_meta('display_name', $contributor_id); ?></h2>
-                        <p class="contributor-bio">
-                            <?php echo get_the_author_meta('description', $contributor_id); ?>
-                        </p>
-                        <a class="contributor-posts-link" href="<?php echo esc_url(get_author_posts_url($contributor_id)); ?>">
-                            <?php printf(_n('%d Article', '%d Articles', $post_count, 'bigblank'), $post_count); ?>
-                        </a>
-                    </div><!-- .contributor-summary -->
-                </div><!-- .contributor-info -->
-            </div><!-- .contributor -->
-            <?php
-        endforeach;
-    }
-
-endif;
 
 /**
  * Extend the default WordPress body classes.
