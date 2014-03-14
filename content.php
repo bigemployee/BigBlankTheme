b40e3502e0a6c1a104a9966d8cf0c2fa0e75c24f
Norik Davtian <norik@bigemployee.com>
2014-03-14 06:52:28 -0700

    author description bio
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- content.php
+++ content.php
@@ -53,5 +53,21 @@
         endif;
         the_tags('<span class="entry-tags">', _x(', ', 'Used between list items, there is a space after the comma.', 'bigblank'), '</span>');
         ?>
+        <?php if (is_singular() && get_the_author_meta('description') && is_multi_author()) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
+            <div class="author-info">
+                <div class="author-avatar">
+                    <?php echo get_avatar(get_the_author_meta('user_email'), apply_filters('gravatar', 128)); ?>
+                </div><!-- .author-avatar -->
+                <div class="author-description">
+                    <h2><?php printf(__('About %s', 'betheme'), get_the_author()); ?></h2>
+                    <p><?php the_author_meta('description'); ?></p>
+                    <div class="author-link">
+                        <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
+                            <?php printf(__('View all posts by %s <span class="meta-nav">&rarr;</span>', 'betheme'), get_the_author()); ?>
+                        </a>
+                    </div><!-- .author-link	-->
+                </div><!-- .author-description -->
+            </div><!-- .author-info -->
+        <?php endif; ?>            
     </footer>
 </article><!-- #post-## -->
