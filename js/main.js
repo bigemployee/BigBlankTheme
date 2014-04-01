f8daf64724086f3bc32895f8d62d146124ccc06c
Norik Davtian <norik@bigemployee.com>
2014-03-31 22:03:43 -0700

    no extra js for now
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@
     /**
      * Handlers for navigation, accessibility, header sizing
      */
-    var body = $('body'),
-            _window = $(window);
+//    var body = $('body'),
+//            _window = $(window);
 
     // Enable menu toggle for small screens.
     (function() {
@@ -49,33 +49,33 @@
      *
      * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
      */
-    _window.on('hashchange.bigblank', function() {
-        var element = document.getElementById(location.hash.substring(1));
-
-        if (element) {
-            if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-                element.tabIndex = -1;
-            }
-
-            element.focus();
-
-            // Repositions the window on jump-to-anchor to account for header height.
-            window.scrollBy(0, -80);
-        }
-    });
+//    _window.on('hashchange', function() {
+//        var element = document.getElementById(location.hash.substring(1));
+//
+//        if (element) {
+//            if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+//                element.tabIndex = -1;
+//            }
+//
+//            element.focus();
+//
+//            // Repositions the window on jump-to-anchor to account for header height.
+//            window.scrollBy(0, -80);
+//        }
+//    });
 
     // Search toggle.
-    $('.search-toggle').on('click.bigblank', function(event) {
-        var that = $(this),
-                wrapper = $('.search-box-wrapper');
-
-        that.toggleClass('active');
-        wrapper.toggleClass('hide');
-
-        if (that.is('.active') || $('.search-toggle .screen-reader-text')[0] === event.target) {
-            wrapper.find('.search-field').focus();
-        }
-    });
+//    $('.search-toggle').on('click.bigblank', function(event) {
+//        var that = $(this),
+//                wrapper = $('.search-box-wrapper');
+//
+//        that.toggleClass('active');
+//        wrapper.toggleClass('hide');
+//
+//        if (that.is('.active') || $('.search-toggle .screen-reader-text')[0] === event.target) {
+//            wrapper.find('.search-field').focus();
+//        }
+//    });
 
     /*
      * Fixed header for large screen.
@@ -84,36 +84,36 @@
      * The callback on the scroll event is only added if there is a header
      * image and we are not on mobile.
      */
-    if (_window.width() > 781) {
-        var mastheadHeight = $('#masthead').height(),
-                toolbarOffset, mastheadOffset;
-
-        if (mastheadHeight > 48) {
-            body.removeClass('masthead-fixed');
-        }
-
-        if (body.is('.header-image')) {
-            toolbarOffset = body.is('.admin-bar') ? $('#wpadminbar').height() : 0;
-            mastheadOffset = $('#masthead').offset().top - toolbarOffset;
-
-            _window.on('scroll.bigblank', function() {
-                if ((window.scrollY > mastheadOffset) && (mastheadHeight < 49)) {
-                    body.addClass('masthead-fixed');
-                } else {
-                    body.removeClass('masthead-fixed');
-                }
-            });
-        }
-    }
+//    if (_window.width() > 781) {
+//        var mastheadHeight = $('#masthead').height(),
+//                toolbarOffset, mastheadOffset;
+//
+//        if (mastheadHeight > 48) {
+//            body.removeClass('masthead-fixed');
+//        }
+//
+//        if (body.is('.header-image')) {
+//            toolbarOffset = body.is('.admin-bar') ? $('#wpadminbar').height() : 0;
+//            mastheadOffset = $('#masthead').offset().top - toolbarOffset;
+//
+//            _window.on('scroll.bigblank', function() {
+//                if ((window.scrollY > mastheadOffset) && (mastheadHeight < 49)) {
+//                    body.addClass('masthead-fixed');
+//                } else {
+//                    body.removeClass('masthead-fixed');
+//                }
+//            });
+//        }
+//    }
 
     // Focus styles for menus.
-    $('.primary-navigation, .secondary-navigation').find('a').on('focus.bigblank blur.bigblank', function() {
+    $('.primary-navigation, .secondary-navigation').find('a').on('focus blur', function() {
         $(this).parents().toggleClass('focus');
     });
 
 
 
-    $(document).on('keydown.bigblank', function(e) {
+    $(document).on('keydown', function(e) {
         var url = false;
 
         // Left arrow key code.
