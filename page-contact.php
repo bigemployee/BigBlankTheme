54d10fc55441eeedc0e27c4c81aae72223fe8741
Norik Davtian <norik@bigemployee.com>
2014-04-16 17:55:27 -0700

    contact page template and flat jquery ui
    Signed-off-by: Norik Davtian <norik@bigemployee.com>

---
+++ page-contact.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Contact Page Template
+ *
+ */
+get_header();
+get_header('layout');
+?>
+<?php
+
+// Start the Loop.
+while (have_posts()) : the_post();
+?>
+<article id="page-contact" <?php post_class(); ?>>
+    <header class="entry-header">
+    <?php
+    echo do_shortcode('[bigContact map=on]');
+    the_title('<h1 class="entry-title">', '</h1>');
+    ?>
+    </header><!-- .entry-header -->
+    <div class="entry-content">
+        <?php
+        the_content();
+        ?>
+    </div><!-- .entry-content -->
+    <footer class="entry-meta">
+        <?php edit_post_link(__('Edit', 'bigblank')); ?>
+    </footer><!-- .entry-meta -->
+</article><!-- #page-contact -->
+<?php
+endwhile;
+?>
+<?php
+
+get_footer('layout');
+get_footer();
