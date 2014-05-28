fe7ec0f8fdd5ed46b118abb0c6a1a79c8fba216a
Norik Davtian <norik@bigemployee.com>
2014-05-27 22:42:22 -0700

    echo the schema

--- header-layout.php
+++ header-layout.php
@@ -7,9 +7,9 @@ $layout = bigblank_get_layout();
 if (is_home() || is_archive()) {
     $schema = schema(false, 'Blog', false);
     if (is_author()) {
-        $schema =  schema(false, 'ProfilePage', false);
+        $schema = schema(false, 'ProfilePage', false);
     }
 }
 ?>
-<div id="content" class="site-content <?php echo $layout; ?>" <?php echo $schema; ?>>
+<div id="content" class="site-content<?php echo $layout ? ' ' . $layout : '' ?>"<?php echo $schema ?>>
     <div id="main" role="main">
